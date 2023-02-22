f6f24ee8d931a693e3ec02588f15669e0e854d2f
pearce-odegard <pearceodegard@gmail.com>
2023-02-22 11:38:21 -0600

    Established many-to-many relationship between Dinos and Colors

--- src/main/java/org/launchcode/Ch15Lecture/models/Color.java
+++ src/main/java/org/launchcode/Ch15Lecture/models/Color.java
@@ -1,13 +1,22 @@
 package org.launchcode.Ch15Lecture.models;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 import javax.validation.constraints.NotBlank;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Color extends AbstractEntity {
 
     @NotBlank
     private String name;
+
+    // Now that we have established the many-to-many relationship on the
+    // Dinosaur class' end, we need to complete that relationship here in
+    // the Color class
+    @ManyToMany(mappedBy = "colors")
+    private List<Dinosaur> dinosaurs = new ArrayList<>();
 
     public Color() { }
 
@@ -21,5 +30,13 @@ public class Color extends AbstractEntity {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<Dinosaur> getDinosaurs() {
+        return dinosaurs;
+    }
+
+    public void setDinosaurs(List<Dinosaur> dinosaurs) {
+        this.dinosaurs = dinosaurs;
     }
 }
